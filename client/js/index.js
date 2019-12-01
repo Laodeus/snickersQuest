fd486df9712e50927b8b3fe50d8ae711218c01c9
Philippe Haerens <haerphi@gmail.com>
2019-12-01 09:59:45 +0100

    gamescene as first scene

--- client/js/index.js
+++ client/js/index.js
@@ -1,20 +1,19 @@
 var config = {
-    type: Phaser.AUTO,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        parent: 'phaser-example',
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 800,
-        height: 600
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
-    },
-    scenes: [landingScene,menuScene,GameScene1]
+  type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  scene: [GameScene1, LandingScene, MenuScene]
 };
 
 var game = new Phaser.Game(config);
