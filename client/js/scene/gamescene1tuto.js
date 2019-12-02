788cf83a23aa1673dede9ed1b8900bd3c602b8f3
Philippe Haerens <haerphi@gmail.com>
2019-12-02 15:15:11 +0100

    some changes

--- client/js/scene/gamescene1tuto.js
+++ client/js/scene/gamescene1tuto.js
@@ -40,7 +40,6 @@ class GameScene1 extends Phaser.Scene {
     //duplicate ground
     this.platforms = this.physics.add.staticGroup();
     this.platforms.create(240, 552, "spritesheet", "platform/floor01.png");
-    this.platforms.create(200, 472, "spritesheet", "platform/cardbox-0001.png");
     let ground = this.platforms
       .create(480 + 128, 504, "spritesheet", "platform/floor01.png")
       .setOrigin(0)
@@ -51,20 +50,28 @@ class GameScene1 extends Phaser.Scene {
     // this.holes = this.physics.add.staticGroup();
     this.platforms.create(544, 576, "spritesheet", "platform/floor_hole.png");
 
-    this.player = new Player(this, 50, 460);
-
     // adding platformGroup
     this.platformUp = new Platforms(this.world, this);
-    
-    // adding an object to the scene
-    this.platforms.create(1200,450,'desk').setOrigin(0).refreshBody();
-    this.platformUp.create(1275,400,'computer').setScale(1.7,1.7).refreshBody();
-    this.platforms.create(650, 472, "box");
-    this.platforms.create(800, 472, "box");
-    this.platforms.create(775, 408, "box");
 
+    // adding an object to the scene
+    this.platforms
+      .create(1200, 450, "spritesheet", "platform/table-0001.png")
+      .setOrigin(0)
+      .refreshBody();
+
+    this.platformUp.createPlatforms(
+      1275,
+      400,
+      "spritesheet",
+      "deco/computer-0001.gif"
+    );
+    this.platforms.create(650, 472, "spritesheet", "platform/cardbox-0001.png");
+    this.platforms.create(800, 472, "spritesheet", "platform/cardbox-0001.png");
+    this.platforms.create(775, 408, "spritesheet", "platform/cardbox-0001.png");
 
     //camera stuff it's not in the player because the camera maybe change if the map is bigger
+
+    this.player = new Player(this, 50, 460);
     this.cameras.main.startFollow(this.player);
     this.cameras.main.setBounds(
       0,
@@ -80,6 +87,7 @@ class GameScene1 extends Phaser.Scene {
     this.physics.add.collider(this.player, this.platforms);
     this.physics.add.collider(this.player, this.world);
     this.physics.add.collider(this.enemy, this.platforms);
+    this.physics.add.collider(this.player, this.platformUp);
   }
 
   update() {
