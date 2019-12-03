d5f4c153211d2570cee5bd8818914c69bdb6116c
Jonathan <atta.jon.t@gmail.com>
2019-12-03 22:06:26 +0100

    adding somes physocs

--- client/js/scene/gamescene2Greece.js
+++ client/js/scene/gamescene2Greece.js
@@ -5,67 +5,120 @@ class GameScene2 extends Phaser.Scene {
   }
 
   preload() {
-      this.load.spritesheet("Leny", "assets/game/leny-sprite.png", {
-        frameWidth: 91,
-        frameHeight: 95
-      });
-      this.load.spritesheet("Julie001", "assets/game/julie001.png", {
-        frameWidth: 86,
-        frameHeight: 120
-      });
-      this.load.spritesheet("colon_idle", "assets/game/colon_idle.png", {
-        frameWidth: 24,
-        frameHeight: 41
-      });
-  
-      this.load.spritesheet("colon", "assets/game/colon.png", {
-        frameWidth: 35,
-        frameHeight: 33
-      });
-  
-      this.load.spritesheet("door", "assets/game/door.png", {
-        frameWidth: 96,
-        frameHeight: 160
-      });
-      this.load.image('ground-tile-Left-Border', 'assets/game/scene2/ground-tile-Left-Border.png');
-      this.load.image('ground-tile', 'assets/game/scene2/ground-tile.png');
-      this.load.image('ground-tile-Right-Border', 'assets/game/scene2/ground-tile-Right-Border.png');
+    this.load.spritesheet("Leny", "assets/game/leny-sprite.png", {
+      frameWidth: 91,
+      frameHeight: 95
+    });
+    this.load.spritesheet("Julie001", "assets/game/julie001.png", {
+      frameWidth: 86,
+      frameHeight: 120
+    });
+    this.load.spritesheet("colon_idle", "assets/game/colon_idle.png", {
+      frameWidth: 24,
+      frameHeight: 41
+    });
+
+    this.load.spritesheet("colon", "assets/game/colon.png", {
+      frameWidth: 35,
+      frameHeight: 33
+    });
+
+    this.load.spritesheet("door", "assets/game/door.png", {
+      frameWidth: 96,
+      frameHeight: 160
+    });
+    this.load.image(
+      "ground-tile-Left-Border",
+      "assets/game/scene2/ground-tile-Left-Border.png"
+    );
+    this.load.image("ground-tile", "assets/game/scene2/ground-tile.png");
+    this.load.image(
+      "ground-tile-Right-Border",
+      "assets/game/scene2/ground-tile-Right-Border.png"
+    );
+    this.load.multiatlas(
+      "spritesheet",
+      "assets/game/spritesheet.json",
+      "assets/game"
+    );
   }
 
   create(data) {
-    
-    this.physics.world.setBounds(0, 0, 3200, 1200)
-    
-    // create plain platform
-    this.platforms = this.physics.add.staticGroup();
-    // addding platform to the world
+    // seting size of scene
+    this.physics.world.setBounds(0, 0, 3200, 1200);
 
-    this.platforms.create(0, 536, "ground-tile-Left-Border").setOrigin(0).refreshBody();
-    this.platforms.create(64, 536, "ground-tile").setOrigin(0).refreshBody();
-    this.platforms.create(128, 536, "ground-tile").setOrigin(0).refreshBody();
-    this.platforms.create(192, 536, "ground-tile").setOrigin(0).refreshBody();
-    this.platforms.create(256, 536, "ground-tile").setOrigin(0).refreshBody();
-    this.platforms.create(320, 536, "ground-tile").setOrigin(0).refreshBody();
-    this.platforms.create(384, 536, "ground-tile").setOrigin(0).refreshBody();
-    this.platforms.create(448, 536, "ground-tile").setOrigin(0).refreshBody();
-    this.platforms.create(512, 536, "ground-tile").setOrigin(0).refreshBody();
-    this.platforms.create(576, 536, "ground-tile").setOrigin(0).refreshBody();
+    // generating group object
+    this.platformUp = new Platforms(this.world, this);
+    this.movingPlatform = new MovingPlatform(this.world, this);
+
+    //generating the first ground ground
+
+    // adding object to the world
+    let i = 0;
+    this.platformUp.createPlatforms(i * 64, 1136, "ground-tile-Left-Border");
+    for (; i < 12; i++) {
+      this.platformUp.createPlatforms(i * 64, 1136, "ground-tile");
+    }
+    this.platformUp.createPlatforms(i * 64, 1136, "ground-tile-Right-Border");
+
+    this.gate = new Gate(
+      this,
+      500,
+      975,
+      "spritesheet",
+      "deco/door-0001.png",
+      "GameScene2"
+    );
+    this.add.sprite(545, 950, "spritesheet", "deco/exitpannel.png");
+    
+    // the player and set it's property
+    this.player = new Player(this, 50, 1080, false);
+    if (data.player) {
+      // if we have a player in the data, we can set the property of this player at this one's. and i know my english is poor phil, no need to say it :p
+    
+    }
+
+    //Here, i will play with the camera stuff ^^
+    this.cameras.main.startFollow(this.player);
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.physics.world.bounds.width,
+      this.physics.world.bounds.height
+    );
     
 
-    
+    // setting the colider
 
-      if(!data.player){
-        
-      }else{
-        this.player = data.player.scene
-        this.player.x = 50;
-        this.player.y = 50;
+    // player colider
+    this.physics.add.collider(this.player, this.platformUp.group);
+    this.physics.add.collider(this.player,this.movingPlatform.group,this.movingPlatform.movingObjectWhitPlatform);
+
+    //enemies colider
+    // this.physics.add.collider(
+    //   this.enemies,
+    //   this.movingPlatform.group,
+    //   this.movingPlatform.movingObjectWhitPlatform
+    // );
+    // this.physics.add.collider(
+    //   this.enemies,
+    //   this.platformUp.group,
+    // );
+    
+    //door colider
+    this.physics.add.collider(this.gate, this.movingPlatform.group);
+    this.physics.add.collider(this.gate, this.platformUp.group);
+    this.physics.add.overlap(this.gate, this.player, (door, player) => {
+      if (!this.doorOverlapped) {
+        this.gate.passTheGate(door, player);
       }
-
-    
-
+      this.doorOverlapped = true;
+    });
   }
 
   update() {
+    this.player.move();
+    //this.enemies.move();
+    this.movingPlatform.move();
   }
 }
